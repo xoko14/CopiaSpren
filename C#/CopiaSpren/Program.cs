65c26cb89bc6cc0a603033ff2102dd3e39dec7b0
Xoko14 <xoquinperezb@gmail.com>
2021-03-02 13:39:36 +0100

    C# init

---
+++ C#/CopiaSpren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopiaSpren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
